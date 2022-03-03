06d1e0cf3eb87e2d819aa4da7578d45cca73ad91
Alexkdevops <alexkad.devops@gmail.com>
2022-03-03 15:42:14 -0600

    new

--- del.sh
+++ del.sh
@@ -1,10 +1,31 @@
 #!/bin/bash
+
+# Delete deploy from all namespaces
+kubectl delete deploy api-deployment -n main
+kubectl delete deploy web-deployment -n main
 kubectl delete deploy api-deployment -n dev
 kubectl delete deploy api-deployment -n prod
 kubectl delete deploy web-deployment -n dev
 kubectl delete deploy web-deployment -n prod
+# Delete SVC from all namespaces
 kubectl delete svc backend -n dev
 kubectl delete svc backend -n prod
 kubectl delete svc frontend -n dev
 kubectl delete svc frontend -n prod
-kubectl delete ns prod dev
+kubectl delete svc backend -n main
+kubectl delete svc frontend -n main
+# Delete Ingress from all namespaces
+kubectl delete ingress frontend-svc -n main
+kubectl delete ingress backend-svc -n main
+kubectl delete ingress frontend-svc -n prod
+kubectl delete ingress backend-svc -n prod
+kubectl delete ingress frontend-svc -n dev
+kubectl delete ingress backend-svc -n dev
+
+# Delete Secrets and Svc for database
+kubectl delete secret credentials -n main
+kubectl delete svc mysql-service -n main
+kubectl delete secret credentials -n dev
+kubectl delete svc mysql-service -n dev
+kubectl delete secret credentials -n prod
+kubectl delete svc mysql-service -n prod
